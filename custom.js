70a9dfac28baeacab72a5cb369dc19226e399796
aaroncf2008 <aaroncf2008@gmail.com>
2021-11-09 16:21:46 -0500

    awfawfawfawfaw
    fawfawf

--- custom.js
+++ custom.js
@@ -55,7 +55,7 @@ window.snake.more_menu = function(e) {
 
   // blursed
   img = new Image;
-  img.src = 'https://cdn.discordapp.com/attachments/723093815786864661/894286619530653726/unknown.png';
+  img.src = 'https://i.postimg.cc/Gp8s489f/unknown.png';
   img.width = 47;
   img.height = 47;
   img.class = 'DqMRee SsAred';
@@ -89,7 +89,7 @@ window.snake.more_menu = function(e) {
 
   // desert bus
   img = new Image;
-  img.src = 'https://cdn.discordapp.com/attachments/723093815786864661/894619150717177886/unknown.png';
+  img.src = 'https://i.postimg.cc/cHfgcpRM/unknown-1.png';
   img.width = 47;
   img.height = 47;
   img.class = 'DqMRee SsAred';
@@ -137,7 +137,7 @@ window.snake.more_menu = function(e) {
 
   // ultradeath
   img = new Image;
-  img.src = 'https://cdn.discordapp.com/attachments/723093815786864661/894620725888040981/unknown.png';
+  img.src = 'https://i.postimg.cc/280qTFZJ/unknown-2.png';
   img.width = 47;
   img.height = 47;
   img.class = 'DqMRee SsAred';
